cbdcd31417493f2f0c8a789aeb1d7c1dd1da812c
buroakscampground-glitch <buroakscampground@gmail.com>
2026-06-23 16:32:41 -0500

    Use local aerial map card

--- components/CampgroundMap.tsx
+++ components/CampgroundMap.tsx
@@ -2,8 +2,7 @@ const campgroundAddress = '10303 Oaks Rd, Alhambra, IL 62001'
 const encodedAddress = encodeURIComponent(campgroundAddress)
 const latitude = 38.8884
 const longitude = -89.7312
-const satelliteImageUrl =
-  'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/export?bbox=-89.7382,38.8838,-89.7242,38.8926&bboxSR=4326&imageSR=4326&size=1200,720&format=jpg&f=image'
+const aerialMapUrl = '/bur-oaks-aerial-map.svg'
 const directionsUrl = `https://www.google.com/maps/dir/?api=1&destination=${encodedAddress}`
 const satelliteUrl = `https://www.google.com/maps/@${latitude},${longitude},700m/data=!3m1!1e3`
 
@@ -17,12 +16,12 @@ export default function CampgroundMap({
   return (
     <div className={`campground-map ${compact ? 'compact' : ''}`}>
       <img
-        alt="Satellite view of Bur Oaks Campground"
-        src={satelliteImageUrl}
+        alt="Aerial style view of Bur Oaks Campground"
+        src={aerialMapUrl}
       />
 
       <div className="campground-map-overlay">
-        <span>Satellite view</span>
+        <span>Aerial site view</span>
         <strong>Bur Oaks Campground</strong>
         <small>{campgroundAddress}</small>
         {lotNumber && <em>Your portal site: Lot {lotNumber}</em>}
@@ -33,7 +32,7 @@ export default function CampgroundMap({
         <a href={satelliteUrl} rel="noreferrer" target="_blank">Satellite view</a>
       </div>
 
-      <small className="campground-map-credit">Satellite imagery © Esri, Maxar, Earthstar Geographics</small>
+      <small className="campground-map-credit">Bur Oaks aerial-style map</small>
     </div>
   )
 }
