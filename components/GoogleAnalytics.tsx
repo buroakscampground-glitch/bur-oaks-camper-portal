9ac50ebe2ee28e2dc3e7cd0940beb29fe096ba45
buroakscampground-glitch <buroakscampground@gmail.com>
2026-07-14 15:14:25 -0500

    Make Analytics initialization deterministic

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,25 +1,9 @@
 'use client'
 
 import Script from 'next/script'
-import { useEffect } from 'react'
-
-type GoogleAnalyticsWindow = Window & {
-  dataLayer?: unknown[]
-  gtag?: (...args: unknown[]) => void
-}
 
 export default function GoogleAnalytics() {
   const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
-
-  useEffect(() => {
-    if (!measurementId) return
-
-    const analyticsWindow = window as GoogleAnalyticsWindow
-    analyticsWindow.dataLayer = analyticsWindow.dataLayer || []
-    analyticsWindow.gtag = (...args: unknown[]) => analyticsWindow.dataLayer?.push(args)
-    analyticsWindow.gtag('js', new Date())
-    analyticsWindow.gtag('config', measurementId, { anonymize_ip: true })
-  }, [measurementId])
 
   if (!measurementId) return null
 
