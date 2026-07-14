64ef10bf8559cf5468683d2927647eb5b161afb8
buroakscampground-glitch <buroakscampground@gmail.com>
2026-07-14 15:12:45 -0500

    Initialize Google Analytics after page load

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,21 +1,27 @@
 'use client'
 
 import Script from 'next/script'
+import { useEffect } from 'react'
+
+type GoogleAnalyticsWindow = Window & {
+  dataLayer?: unknown[]
+  gtag?: (...args: unknown[]) => void
+}
 
 export default function GoogleAnalytics() {
   const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 
+  useEffect(() => {
+    if (!measurementId) return
+
+    const analyticsWindow = window as GoogleAnalyticsWindow
+    analyticsWindow.dataLayer = analyticsWindow.dataLayer || []
+    analyticsWindow.gtag = (...args: unknown[]) => analyticsWindow.dataLayer?.push(args)
+    analyticsWindow.gtag('js', new Date())
+    analyticsWindow.gtag('config', measurementId, { anonymize_ip: true })
+  }, [measurementId])
+
   if (!measurementId) return null
 
-  return (
-    <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`} strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', '${measurementId}', { anonymize_ip: true });`}
-      </Script>
-    </>
-  )
+  return <Script src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`} strategy="afterInteractive" />
 }
